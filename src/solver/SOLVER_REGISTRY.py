d0b1e717eae3f34c0a40d1272b8213ab02103fbd
Zihan Shao <eddyszh@outlook.com>
2025-11-18 23:50:19 -0800

    update registry

--- src/solver/SOLVER_REGISTRY.py
+++ src/solver/SOLVER_REGISTRY.py
@@ -1,7 +1,7 @@
 # src/solver_registry.py
-from src.sovler.solver import solve as solve_full
-from src.sovler.solver_outer import solve_outer
-from src.sovler.solver_outer_first_order import solve_outer_first_order
+from src.solver.solver import solve as solve_full
+from src.solver.solver_outer import solve_outer
+from src.solver.solver_outer_first_order import solve_outer_first_order
 
 SOLVER_REGISTRY = {
     "full": solve_full,
