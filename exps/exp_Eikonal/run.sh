08129988b3f7dfbdc0cb3ec6a89b6f6d7cbff438
Zihan Shao <eddyszh@outlook.com>
2025-12-08 22:44:47 -0600

    add eikonal

---
+++ exps/exp_Eikonal/run.sh
@@ -0,0 +1,5 @@
+# python scripts/solve_pde.py --config exps/exp_2D/first_order.yaml --seed 200 --save_dir exps/exp_2D/first_order_results
+# python scripts/solve_pde.py --config exps/exp_2D/second_order_full.yaml --seed 200 --save_dir exps/exp_2D/second_order_full_results
+# python scripts/solve_pde.py --config exps/exp_2D/second_order_outer.yaml --seed 200 --save_dir exps/exp_2D/second_order_outer_results
+# python scripts/solve_pde.py --config exps/exp_2D/first_order_full.yaml --seed 200 --save_dir exps/exp_2D/first_order_full_results
+python scripts/solve_pde.py --config exps/exp_Eikonal/second_order_full.yaml --seed 200 --save_dir exps/exp_Eikonal/second_order_full_results
